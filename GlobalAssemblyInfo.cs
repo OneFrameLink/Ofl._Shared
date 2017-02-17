5daca7fcf68e5656aeea1c3b3ed41e81a2dd83a8
Nicholas Paldino <casperOne@caspershouse.com>
2017-02-17 15:12:06 -0500

    Version update.
    Updated shared version info attributes.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -18,3 +18,9 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// Assembly version.
+// NOTE: These are currently replaced in the automated build script.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
